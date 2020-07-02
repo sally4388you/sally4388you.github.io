7b269bfc5bdb09ca623cae2015db766820b1a09f
sally <sally4388you@126.com>
2020-07-01 22:13:29 -0700

    added line numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,6 @@
 var leftTab = document.getElementById('left-tab'),
     rightTab = document.getElementById('right-tab');
 
-// 垃圾方法，该方法必须等页面加载玩才有效
-// window.onload = function(){
-//   btn[0].onmouseover=function() {
-//     leftTab.style.display="block";
-//     leftTab.setAttribute("class","rotateInUpLeft self-animated");
-//   }
-//   btn[0].onmouseout=function() {
-//     leftTab.setAttribute("class","rotateOutDownLeft self-animated");
-//     // leftTab.style.display="none";
-//   }
-//   btn[1].onmouseover=function() {
-//     rightTab.style.display="block";
-//     rightTab.setAttribute("class","rotateInUpRight self-animated");
-//   }
-//   btn[1].onmouseout=function() {
-//     rightTab.setAttribute("class","rotateOutDownRight self-animated");
-//     // leftTab.style.display="none";
-//   }
-//
-// }
-
 function showLeft() {
   leftTab.style.display="block";
   leftTab.setAttribute("class","rotateInUpLeft self-animated");
@@ -38,4 +17,23 @@ function showRight() {
 
 function goneRight() {
   rightTab.setAttribute("class","rotateOutDownRight self-animated");
+}
+
+
+// add line numbers for code snippet
+
+let code_snippets = document.querySelectorAll('pre.highlight');
+
+for (let i = 0; i < code_snippets.length; i ++) {
+    let code = code_snippets[i].children[0];
+    let total_lines = Math.ceil(parseInt(code.offsetHeight) / 21);
+
+    let wrapper = document.createElement('span');
+    wrapper.setAttribute('class', 'line-numbers-rows');
+    wrapper.setAttribute('aria-hidden', 'hidden');
+    for (let j = 1; j <= total_lines; j++) {
+        wrapper.appendChild(document.createElement('span'));
+    }
+    
+    code.appendChild(wrapper);
 }
